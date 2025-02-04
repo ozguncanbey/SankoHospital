78ae7a84d79c4cf62859e51c657050ac6f8033c5
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2025-02-04 14:04:03 +0300

    create foldering

---
+++ SankoHospital.MvcWebUITests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SankoHospital.MvcWebUITests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
